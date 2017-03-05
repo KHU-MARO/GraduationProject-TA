e9f6aeed3c3e1097a6a74f447b24a626279af371
sauber92 <jjy920517@gmail.com>
2017-03-05 17:25:05 +0900

    Upload files

--- renderer.js
+++ renderer.js
@@ -1,3 +1,22 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+var e = $.Event("keydown");
+var saveKey = e.ctrlKey && e.which == 83;
+var saveKeyCode = e.ctrlKey && e.keyCode == 83;
+
+$(document).keydown(function(event){
+  if(event.keyCode == saveKeyCode){
+    alert("keypressed");
+  }
+});
+
+function Start() {
+  saveKey = e.ctrlKey && e.which == 83;
+  $(document).trigger(saveKey);
+}
+
+//
+// $(window).keypress(function(event) {
+//   if (!(event.which == 115 && event.ctrlKey) && !(event.which == 19)) return true;
+//   alert("Ctrl-S pressed");
+//   event.preventDefault();
+//   return false;
+// });
