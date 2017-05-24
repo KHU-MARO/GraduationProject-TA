1ec0356556ba3cb629d03759f5e768680a1eb3dd
sauber92 <jjy920517@gmail.com>
2017-05-24 23:29:22 +0900

    아두이노 메가와 릴레이를 TA에서 제어

--- public/js/renderer.js
+++ public/js/renderer.js
@@ -7,7 +7,17 @@ var countDownTimer;
 var time, score, fileSection;
 var startButton;
 var letter, letterToNum;
-var state = false;
+
+var state1 = false;
+var state2 = false;
+var state3 = false;
+var state4 = false;
+var state5 = false;
+var state6 = false;
+var state7 = false;
+var state8 = false;
+var state9 = false;
+var state10 = false;
 
 util.inherits(MyStream, Readable);
 function MyStream(opt) {
@@ -29,14 +39,48 @@ function placeLetter() {
   fileSection.appendChild(newLetter);
 
   letterToNum = letter.charCodeAt(0);
-  if(letterToNum == 97 || letterToNum == 104 || letterToNum == 107 || letterToNum == 110 || letterToNum == 112
-    || letterToNum == 114 || letterToNum == 117 || letterToNum == 118 || letterToNum == 119 || letterToNum == 120) {
-    state = true;
+  if(letterToNum == 97) {
+    state1 = true;
+  }
+  else if(letterToNum == 104) {
+    state2 = true;
+  }
+  else if(letterToNum == 107) {
+    state3 = true;
+  }
+  else if(letterToNum == 110) {
+    state4 = true;
+  }
+  else if(letterToNum == 112) {
+    state5 = true;
+  }
+  else if(letterToNum == 114) {
+    state6 = true;
+  }
+  else if(letterToNum == 117) {
+    state7 = true;
+  }
+  else if(letterToNum == 118) {
+    state8 = true;
+  }
+  else if(letterToNum == 119) {
+    state9 = true;
+  }
+  else if(letterToNum == 120) {
+    state10 = true;
   }
   else {
-    state = false;
+    state1 = false;
+    state2 = false;
+    state3 = false;
+    state4 = false;
+    state5 = false;
+    state6 = false;
+    state7 = false;
+    state8 = false;
+    state9 = false;
+    state10 = false;
   }
-  // console.log(0);
   console.log(letterToNum);
 }
 
@@ -92,16 +136,73 @@ document.addEventListener("DOMContentLoaded", function(event) {
 });
 
 board.on('ready', function() {
-  var led = new five.Led(13);
   startButton.className = "connect";
   startButton.disabled = false;
 
-  document.addEventListener('keydown', function() {
-    if(state) {
-      led.on();
+  var relay1 = new five.Pin(23);
+  var relay2 = new five.Pin(25);
+  var relay3 = new five.Pin(27);
+  var relay4 = new five.Pin(29);
+  var relay5 = new five.Pin(31);
+  var relay6 = new five.Pin(22);
+  var relay7 = new five.Pin(24);
+  var relay8 = new five.Pin(26);
+  var relay9 = new five.Pin(28);
+  var relay10 = new five.Pin(30);
+
+  relay1.high();
+  relay2.high();
+  relay3.high();
+  relay4.high();
+  relay5.high();
+  relay6.high();
+  relay7.high();
+  relay8.high();
+  relay9.high();
+  relay10.high();
+
+  document.addEventListener('keyup', function() {
+    if(state1) {
+      relay1.low();
+    }
+    else if(state2) {
+      relay2.low();
+    }
+    else if(state3) {
+      relay3.low();
+    }
+    else if(state4) {
+      relay4.low();
+    }
+    else if(state5) {
+      relay5.low();
+    }
+    else if(state6) {
+      relay6.low();
+    }
+    else if(state7) {
+      relay7.low();
+    }
+    else if(state8) {
+      relay8.low();
+    }
+    else if(state9) {
+      relay9.low();
+    }
+    else if(state10) {
+      relay10.low();
     }
     else {
-      led.off();
+      relay1.high();
+      relay2.high();
+      relay3.high();
+      relay4.high();
+      relay5.high();
+      relay6.high();
+      relay7.high();
+      relay8.high();
+      relay9.high();
+      relay10.high();
     }
   });
 });
