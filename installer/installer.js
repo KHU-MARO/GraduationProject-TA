820db7388a389c708b6868da9a010292e249e7e9
sauber92 <jjy920517@gmail.com>
2017-05-31 13:18:20 +0900

    package.json 수정 npm scripts를 사용하여 실행파일 및 설치파일 생성

--- installer/installer.js
+++ installer/installer.js
@@ -1,25 +1,16 @@
-const createWindowsInstaller = require('electron-winstaller').createWindowsInstaller;
-const path = require('path');
+var electronInstaller = require('electron-winstaller');
 
-getInstallerConfig()
-  .then(createWindowsInstaller)
-  .catch((error) => {
-    console.error(error.message || error);
-    process.exit(1);
-  });
+resultPromise = electronInstaller.createWindowsInstaller({
+  appDirectory: './dist/team608-win32-x64',
+  authors: 'Junyoung Jung(sauber92)',
+  outputDirectory: './dist/installer-win32-x64',
+  exe: 'team608.exe',
+  setupExe: 'team608-Setup.exe',
+  setupIcon: './public/img/icons/Icon.ico'
+});
 
-function getInstallerConfig () {
-  console.log('creating windows installer');
-  const rootPath = path.join('./');
-  const outPath = path.join(rootPath, 'dist');
-
-  return Promise.resolve({
-    appDirectory: path.join(outPath, 'team608-win32-x64'),
-    authors: 'Junyoung Jung(sauber92)',
-    noMsi: true,
-    outputDirectory: path.join(outPath, 'installer-win32-x64'),
-    exe: 'team608.exe',
-    setupExe: 'team608Setup.exe',
-    setupIcon: path.join(rootPath, 'public', 'img', 'icons', 'Icon.ico')
-  })
-}
+resultPromise.then(function () {
+    console.log("It worked!");
+  }, function (e) {
+  console.log('No dice: ' + e.message);
+});
